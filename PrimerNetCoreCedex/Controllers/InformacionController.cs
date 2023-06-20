52a93a1d7d7d8b653c3cf6ad6bfed6a0e0b687d4
serraguti <pacoserranox@gmail.com>
2023-06-20 10:30:11 +0200

    commit

--- PrimerNetCoreCedex/Controllers/InformacionController.cs
+++ PrimerNetCoreCedex/Controllers/InformacionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PrimerNetCoreCedex.Models;
 
 namespace PrimerNetCoreCedex.Controllers
 {
@@ -8,6 +9,35 @@ namespace PrimerNetCoreCedex.Controllers
         {
             ViewData["NOMBRE"] = "Alumno martes";
             ViewData["EDAD"] = 41;
+            Persona persona = new Persona();
+            persona.Nombre = "Persona Model";
+            persona.Edad = 16;
+            persona.Email = "persona@gmail.com";
+            return View(persona);
+        }
+
+        public IActionResult ViewControllerGet
+            (string app, string tecnologia)
+        {
+            ViewData["DATOS"] = "Application " + app + " " + tecnologia;
+            return View();
+        }
+
+        //NECESITAMOS DOS METODOS
+        [HttpGet]
+        public IActionResult ViewControllerPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ViewControllerPost
+            (Persona persona)
+        {
+            ViewData["MENSAJE"] =
+                "Nombre: " + persona.Nombre + ", " + 
+                persona.Email
+                + ", Edad: " + persona.Edad;
             return View();
         }
     }
